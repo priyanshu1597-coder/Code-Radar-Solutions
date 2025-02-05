d6b468e9ab0a53eefec8783155b053e2b45cde4b
priyanshu1597-coder <priyanshu1597.becse24@chitkara.edu.in>
2025-02-05 17:17:24 +0530

    Solution to Perform Bitwise OR

--- Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, result;
-
-    // Input two integers
-    printf("Enter first integer: ");
-    scanf("%d", &num1);
-    printf("Enter second integer: ");
-    scanf("%d", &num2);
-
-    // Perform bitwise OR operation
+    int num1 = 5; 
+    int num2 = 3;  
+    int result;
     result = num1 | num2;
-
-    // Print the result
     printf("The result of bitwise OR operation between %d and %d is: %d\n", num1, num2, result);
 
     return 0;
