1f8f204452e43b456f6d703e88fc5e36b16384f0
PRIYANSHU <priyanshusolanki600@gmail.com>
2025-04-08 16:36:33 +0530

    Solution to Print an Alphabet Right-Angled Triangle

--- Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
+++ Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
+void alphabet_triangle(int n) {
+    // Loop through rows from 1 to n
+    for (int i = 1; i <= n; i++) {
+        // Print alphabets from 'A' to the ith alphabet
+        for (int j = 1; j <= i; j++) {
+            // Print the corresponding alphabet
+            printf("%c ", 'A' + j - 1);
+        }
+        printf("\n");  // Move to the next line after each row
+    }
+}
+
 int main() {
     int n;
+    // Input: Number of rows
     printf("Enter the number of rows: ");
     scanf("%d", &n);
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 0; j < i; j++) {
-            printf("%c ", 'A' + j);
-        }
-        printf("\n");
-    }
-
+    
+    // Call the function to print the alphabet triangle
+    alphabet_triangle(n);
+    
     return 0;
 }
